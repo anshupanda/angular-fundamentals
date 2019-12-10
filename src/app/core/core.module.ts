b01dae55139001dddea46971d8bc7bb1cdff647d
Nagarjun <w3.nagarjuna@gmail.com>
2019-12-10 10:20:00 +0530

    removed common module from coremodule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { UtilService } from './util.service';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
   providers: [UtilService]
 })
 export class CoreModule { }
